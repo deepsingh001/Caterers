19e91df9b58145f0b706586413c53903fcbcb6b9
macre <macre@DESKTOP-TIUCGKV>
2026-03-11 18:44:51 +0530

    first commit

---
+++ FoodMahal/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using FoodMahal.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FoodMahal.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
